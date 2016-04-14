abe6aa2102fe75145ccbc701a66fd86d82219a25
max <kwon@yejune.com>
2016-04-14 05:16:02 -0400

    localhost  설정파일 수정

--- app/config/localhost.php
+++ app/config/localhost.php
@@ -1,5 +1,21 @@
 <?php
 
+{
+    define('LANG', 'ko');
+    date_default_timezone_set('ROK');
+    ini_set('display_errors', 'On');
+    ini_set('display_startup_errors', 'On');
+    ini_set('error_reporting', E_ALL);
+    ini_set('log_errors', 'On');
+    ini_set('track_errors', 'On');
+    ini_set('session.cookie_lifetime', '0');
+    ini_set('session.cookie_path', '/');
+    if (false == in_array(getenv('REMOTE_ADDR'), array('114.206.74.108', '106.242.29.130'))) {
+        //header("HTTP/1.0 404 Not Found");
+        //exit();
+    }
+}
+
 $di = new Phalcon\Di\FactoryDefault();
 
 $di->set('session', function () {
