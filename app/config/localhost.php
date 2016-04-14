4f1414b1c3a99b3034e7892610ed58d2527ba5f3
max <kwon@yejune.com>
2016-04-14 05:23:37 -0400

    test

--- app/config/localhost.php
+++ app/config/localhost.php
@@ -10,6 +10,7 @@
     ini_set('track_errors', 'On');
     ini_set('session.cookie_lifetime', '0');
     ini_set('session.cookie_path', '/');
+
     if (false == in_array(getenv('REMOTE_ADDR'), array('000.000.000.000'))) {
         //header("HTTP/1.0 404 Not Found");
         //exit();
