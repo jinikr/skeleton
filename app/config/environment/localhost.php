32e85d41c3f07e7921f55c4e34e5f19645bee3e4
Max <smkwon@yellotravel.com>
2016-04-23 02:13:27 +0900

    테스트 코드 주석 처리

--- app/config/environment/localhost.php
+++ app/config/environment/localhost.php
@@ -20,7 +20,10 @@ return [
             'options'  => [
                 \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
                 \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-                \PDO::ATTR_EMULATE_PREPARES => false
+                \PDO::ATTR_EMULATE_PREPARES => false,
+                \PDO::ATTR_PERSISTENT => false,
+                //\PDO::ATTR_AUTOCOMMIT => false,
+                //\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"
             ]
         ],
         'slave' => [
@@ -31,7 +34,10 @@ return [
             'options'  => [
                 \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
                 \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-                \PDO::ATTR_EMULATE_PREPARES => false
+                \PDO::ATTR_EMULATE_PREPARES => false,
+                \PDO::ATTR_PERSISTENT => false,
+                //\PDO::ATTR_AUTOCOMMIT => false,
+                //\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"
             ]
         ],
     ],
