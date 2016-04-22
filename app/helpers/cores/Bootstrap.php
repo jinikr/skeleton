ea58c6069445f6e436e702ad9b800eb62dd241ce
Max <smkwon@yellotravel.com>
2016-04-23 01:49:04 +0900

    collection  제거  route  추가

--- app/helpers/cores/Bootstrap.php
+++ app/helpers/cores/Bootstrap.php
@@ -18,10 +18,10 @@ class Bootstrap
     {
         try
         {
-            if(true === is_file(__BASE__.'/app/config/environment.php'))
+            if (true === is_file(__BASE__.'/app/config/environment.php'))
             {
                 $environmentsConfig = include_once __BASE__.'/app/config/environment.php';
-                if(true === is_array($environmentsConfig)
+                if (true === is_array($environmentsConfig)
                     && true === isset($environmentsConfig['domains'])
                     && true === is_array($environmentsConfig['domains']))
                 {
@@ -35,14 +35,14 @@ class Bootstrap
                             break;
                         }
                     }
-                    if(true === isset($environmentConfig)
+                    if (true === isset($environmentConfig)
                         && true === is_array($environmentConfig))
                     {
                         $this->config = array_merge($environmentsConfig, $environmentConfig);
                     }
                 }
             }
-            if(!$this->config)
+            if (!$this->config)
             {
                 throw new \Exception(__BASE__.'/app/config/environment.php 을 확인하세요.');
             }
@@ -70,7 +70,7 @@ class Bootstrap
             $params = [];
             $paramsStr = isset($_GET['_url']) ? $_GET['_url'] : '/';
             $strParams = trim($paramsStr, '/');
-            if($strParams !== "")
+            if ($strParams !== "")
             {
                 $params = explode("/", $strParams);
             }
@@ -84,14 +84,84 @@ class Bootstrap
         }
     }
 
+    private function initRoute(\Phalcon\Mvc\Micro $app)
+    {
+
+        $app->param('id', '\App\Controllers\V2->checkId');
+        $app->before('\App\Controllers\V2->before');
+        $app->after('\App\Controllers\V2->after');
+
+        $app->group('v1', function()
+        {
+            $this->param('name', '\App\Controllers\V2->checkName');
+            $this->get('info/{name}', '\App\Controllers\V2->getInfo');
+            $this->post('info/{name}', '\App\Controllers\V2->postInfo');
+            $this->delete('info/{name}', '\App\Controllers\V2->deleteInfo');
+            $this->map('all', '\App\Controllers\V2->getInfo');
+            $this->after('\App\Controllers\V2->after');
+            $this->before('\App\Controllers\V2->before');
+
+            $this->group('new', function()
+            {
+                $this->after('\App\Controllers\V2->after');
+                $this->before('\App\Controllers\V2->before');
+
+                $this->map('', '\App\Controllers\V2\New->getInfo');
+                $this->map('all/{name}', '\App\Controllers\V2\New->getInfo');
+            });
+        });
+        $app->group('v2', function()
+        {
+            $this->param('name', '\App\Controllers\V2->checkName');
+            $this->get('', '\App\Controllers\V2->index');
+
+            $this->group('info', function () {
+                $this->before('\App\Controllers\V2->before');
+                $this->get('{name:[0-9a-zA-Z\-]{5,10}}.{ext}', '\App\Controllers\V2->getInfo');
+            });
+            $this->post('info2/{name}/', '\App\Controllers\V2->postInfo');
+            $this->delete('info2/{name}', '\App\Controllers\V2->deleteInfo');
+            $this->map('all2', '\App\Controllers\V2->getInfo');
+
+            $this->group('new3', function()
+            {
+                $this->map('', '\App\Controllers\V2\New->getInfo');
+                $this->map('all2/{name}', '\App\Controllers\V2\New->getInfo');
+                $this->before('\App\Controllers\V2->before');
+
+                $this->group('new211', function()
+                {
+                    $this->map('', '\App\Controllers\V2\New->getInfo');
+                    $this->map('all2/{name}', '\App\Controllers\V2\New->getInfo');
+                    $this->before('\App\Controllers\V2->before');
+                });
+            });
+        });
+        $app->get   ('{name}', '\App\Controllers\V2->index');
+
+        $app->notFound(
+            function () use ($app)
+            {
+                $app->response->setStatusCode(404, 'Not Found');
+                $app->response->setContent('404 Page or File Not Found');
+                return $app->response;
+            }
+        );
+        $app->error(
+            function ($e) use ($app)
+            {
+                pr($e);
+            }
+        );
+    }
+
     private function initDatabase()
     {
-        if(true === isset($this->config['databases']))
+        if (true === isset($this->config['databases']))
         {
             \Peanut\Db\Driver::setConnectInfo($this->config['databases']);
         }
     }
-
 
     private function initRequest()
     {
@@ -105,16 +175,13 @@ class Bootstrap
     {
         $this->initConfig();
         $this->initSession();
-        $this->initCollection();
+        //$this->initCollection();
         $this->initDatabase();
         $this->initRequest();
+        $this->initRoute($app);
 
         $app->setDI($this->di);
 
-        if($this->collection)
-        {
-            $app->mount($this->collection);
-        }
         return $app;
     }
 
@@ -122,4 +189,5 @@ class Bootstrap
     {
         return $this->run($app);
     }
+
 }
